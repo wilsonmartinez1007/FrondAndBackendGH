d2cec62a352c664899e9a9a55d2ddb73b08c7424
juangabrielparedes <juan.gabriel.paredes@correounivalle.edu.co>
2024-12-10 21:27:31 -0500

    cambieserializer

--- Back_End/user/serializers.py
+++ Back_End/user/serializers.py
@@ -1,18 +1,18 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 
-
 class UserSerializer(serializers.ModelSerializer):
     email = serializers.EmailField()
     password2 = serializers.CharField(write_only=True)
     password = serializers.CharField(write_only=True)
-    
+    cedula = serializers.CharField(max_length=15, required=True)  # Nuevo campo para la cédula
+
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'password', 'password2')
-#123
+        fields = ('id', 'username', 'email', 'password', 'password2', 'cedula')
+
     def validate_email(self, email):
-        if User.objects.filter(email=email):
+        if User.objects.filter(email=email).exists():
             raise serializers.ValidationError("Email already exists")
         return email
 
@@ -21,13 +21,13 @@ class UserSerializer(serializers.ModelSerializer):
         if password1 and password2 and password1 != password2:
             raise serializers.ValidationError("Passwords mismatched")
         return password2
-    
+
     def create(self, validated_data):
-        instance = User.objects.create(
-            username=validated_data.get("username"),
-            email=validated_data.get("email"),
-            password=validated_data.get("password2")
-        )
-        instance.set_password(validated_data.get("password"))
+        # Eliminar 'password2' del validated_data antes de usarlo para crear el usuario
+        password = validated_data.pop('password')
+        password2 = validated_data.pop('password2')
+
+        instance = User.objects.create(**validated_data)
+        instance.set_password(password)  # Cifra la contraseña
         instance.save()
         return instance
